8a7566b61e5c4e903c937231cdb93a329e23d082
Mickael Carl <contact+git@carlm.org>
2019-07-30 23:17:37 +0200

    Add Jsonnet support
    The bb-browser service can now accept Jsonnet-based configuration files
    on top of the existing json capability.

--- pkg/configuration/configuration.go
+++ pkg/configuration/configuration.go
@@ -1,32 +1,25 @@
 package configuration
 
 import (
-	"os"
-
 	pb "github.com/buildbarn/bb-browser/pkg/proto/configuration/bb_browser"
-	"github.com/golang/protobuf/jsonpb"
+	"github.com/buildbarn/bb-storage/pkg/util"
 )
 
 // GetBrowserConfiguration reads the configuration from file and fill in default values.
 func GetBrowserConfiguration(path string) (*pb.BrowserConfiguration, error) {
-	file, err := os.Open(path)
-	if err != nil {
-		return nil, err
-	}
 	var browserConfiguration pb.BrowserConfiguration
-	if err := jsonpb.Unmarshal(file, &browserConfiguration); err != nil {
-		return nil, err
+	if err := util.UnmarshalConfigurationFromFile(path, &browserConfiguration); err != nil {
+		return nil, util.StatusWrap(err, "Failed to retrieve configuration")
 	}
 	setDefaultBrowserValues(&browserConfiguration)
-	return &browserConfiguration, err
+	return &browserConfiguration, nil
 }
 
 func setDefaultBrowserValues(browserConfiguration *pb.BrowserConfiguration) {
 	if browserConfiguration.MaximumMessageSizeBytes == 0 {
-		browserConfiguration.MaximumMessageSizeBytes = 16*1024*1024
+		browserConfiguration.MaximumMessageSizeBytes = 16 * 1024 * 1024
 	}
 	if browserConfiguration.ListenAddress == "" {
 		browserConfiguration.ListenAddress = ":80"
 	}
 }
-
